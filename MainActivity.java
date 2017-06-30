650bfc18781e312d7a07c81f69129a26df31bb4e
roguebeetle <roguebeetle@gmail.com>
2017-06-30 23:30:39 +0300

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.evm.kip;
+
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
